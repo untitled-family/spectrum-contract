87eb2b636e2aecba1b08cde72d2233b4bf62b791
Max Bonhomme <hello.bonhomme@gmail.com>
2022-04-14 23:14:09 +0100

    deploy to polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,39 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
 
-const { ALCHEMY_URL, ALCHEMY_KEY, PRIVATE_KEY_DEV } = process.env;
+const {
+  ALCHEMY_URL,
+  ALCHEMY_KEY,
+  PRIVATE_KEY_DEV,
+  PRIVATE_KEY_POLYGON,
+  ALCHEMY_KEY_POLYGON,
+  ALCHEMY_URL_POLYGON,
+  POLYGONSCAN_KEY
+} = process.env;
 
 module.exports = {
   solidity: "0.8.12",
+  // networks: {
+  //   rinkeby: {
+  //     url: ALCHEMY_URL + ALCHEMY_KEY,
+  //     accounts: [`${PRIVATE_KEY_DEV}`],
+  //   },
+  // },
+  defaultNetwork: "matic",
   networks: {
-    rinkeby: {
-      url: ALCHEMY_URL + ALCHEMY_KEY,
-      accounts: [`${PRIVATE_KEY_DEV}`],
+    hardhat: {
     },
+    matic: {
+      url: ALCHEMY_URL_POLYGON + ALCHEMY_KEY_POLYGON,
+      accounts: [`${PRIVATE_KEY_POLYGON}`]
+    }
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: POLYGONSCAN_KEY
+  }
 };
+
+// deployed contract on Polygon
+// 0x721cC02712c3343d2fbfCfde512F247D94967EE4
